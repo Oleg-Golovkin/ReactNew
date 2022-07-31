5276685d1aea8d9ddd840fc207808d63a717101e
Oleg <olegjan9@rambler.ru>
2022-07-31 11:32:12 +0300

    31.07.2022

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect, useCallback} from "react";
+import React, { Component} from "react";
 
 import "./app.css";
 
@@ -150,50 +150,7 @@ class App extends Component {
     }
 
     
-    render(){
-        const Aa = () => {
-            const [years, setYears] = useState(1)
-            const [one, setOne] = useState(0)
-            
-            const getImages = useCallback(()=> {
-                console.log('callBack');
-                return(
-                    [
-                        'https://avatars.mds.yandex.net/get-zen_doc/5221947/pub_6117bb937e37175eb6409d6b_6117bc4e6eab3f04dedc5031/scale_1200',
-                        'https://mobimg.b-cdn.net/v3/fetch/ec/ec1a200599b8bdbed492c7d3ee282d6e.jpeg?w=1200&r=0.5625'
-                    ]
-                )
-            }, [])
-
-            function nextYears() {
-                setYears((years)=> years + 1)
-            }
-            return (
-                <div>   
-                    <button onClick={nextYears}>+++</button>
-                    <div>{years}</div>
-                    <Img getImages={getImages}/>
-                </div>
-            )            
-        }
-
-        const Img = ({getImages}) => {
-            const [pucture, setImg] = useEffect([]);
-            useEffect(() => {
-                setImg(getImages())  
-            }, [getImages])
-
-            return(
-                <div>
-                    {pucture.map((img, i)=> {
-                        return(
-                            <img key={i} src={img} alt="" />
-                        )
-                    })}
-                </div>
-            )
-        }
-        
+    render(){       
         const {data, term, filter} = this.state;
         // Происходит двойной фильтр. Первый аргумент в filterWroker,
         // это уже отфильтрованное состояние в функции searchEmp (первая
@@ -231,10 +188,7 @@ class App extends Component {
                 />       
                 <EmployersAddForm 
                 onAddWorker = {(e, newWorker) => {this.addWorker(e, newWorker)}}
-                />
-                <Aa>
-                    
-                </Aa>
+                />                
             </div>
         );
     }

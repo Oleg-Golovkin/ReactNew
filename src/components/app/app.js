88cf13f1da0f1014b46fbe8c61d740da3d9a5051
Oleg <olegjan9@rambler.ru>
2022-08-01 19:10:32 +0300

    01.08.2022

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,9 @@ import React, { Component} from "react";
 
 import "./app.css";
 
+import Container from 'react-bootstrap/Container';
+import { useState } from "react";
+
 import AppInfo from "../app-info/app-info"
 import SearchPanel from "../search-panel/search-panel"
 import AppFilter from "../app-filter/app-filter"
@@ -150,7 +153,46 @@ class App extends Component {
     }
 
     
-    render(){       
+    render(){
+        const Form = () => {
+            // валидация только буквы
+            // запись состояния
+            const setInputs = (startValue) => {
+                const [text, setText] = useState(startValue);
+
+                setValue = (startValue) => {
+                    setText(startValue)
+                }
+
+                validateValue = (textInput) => {
+                    return textInput.search(/\d/) >= 0
+                }
+
+
+                // return {setValue, text}
+
+            }
+
+
+
+            return (
+                <Container>
+                    <form className="w-50 border mt-5 p-3 m-auto">
+                        <div className="mb-3">
+                            {/* <input value={text} type="text" className="form-control" readOnly/> */}
+                            <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+                            {/* <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/> */}
+                            </div>
+                            <div className="mb-3">
+                            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    </form>
+                </Container>
+            )
+        }
+
+
         const {data, term, filter} = this.state;
         // Происходит двойной фильтр. Первый аргумент в filterWroker,
         // это уже отфильтрованное состояние в функции searchEmp (первая
@@ -188,7 +230,8 @@ class App extends Component {
                 />       
                 <EmployersAddForm 
                 onAddWorker = {(e, newWorker) => {this.addWorker(e, newWorker)}}
-                />                
+                />
+                <Form/>
             </div>
         );
     }

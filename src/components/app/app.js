c7899c56e3571c105057b5d8da503b67af6d065f
Oleg <olegjan9@rambler.ru>
2022-08-27 17:35:30 +0300

    27.08.2022

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import {useReducer} from "react";
 
 const App = () => {
     const [years, dispatch] = useReducer(nextYears, {years: 0});
+    console.log(years);
 
     function nextYears(state, action) {
         console.log(state);
@@ -10,22 +11,20 @@ const App = () => {
             case "one": 
                 return {years: state.years + 1};
             case "two": 
-                return {years: state.years + 2} 
+                return {years: state.years + action.play} 
             case "three": 
                 return {years: state.years + 3} 
-            case "three": 
-                return {years: state.years + 3} 
+            
         }
     }
     return (
         <> 
-            <button onClick={()=> dispatch({type: "one"})}>play</button>
+            <button onClick={()=> dispatch({type: "one"})}>one</button>
             <button onClick={
-                (e)=> dispatch(  
-                                      
-                    {play: e.target.textContent}                    
+                (e)=> dispatch(                    
+                    {type: "two", play: +e.target.textContent},                                       
                 )
-        }>two</button>
+            }>100</button>
             <button onClick={()=> dispatch({type: "three"})}>three</button>
             <div>{years.years}</div>
         </>

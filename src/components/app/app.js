e7aa5eb07facdbea0655503822364ce72cbb9955
Oleg <olegjan9@rambler.ru>
2022-08-28 17:44:44 +0300

    28.08.2022

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,96 @@
+import {useState, useEffect} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
 
-import {useReducer} from "react";
 
-const App = () => {
-    const [years, dispatch] = useReducer(nextYears, {years: 0});
-    console.log(years);
+const getDataFromFirstFetch = () => {return 10};
+const getDataFromSecondFetch = () => {return 20};
 
-    function nextYears(state, action) {
-        console.log(state);
-        switch (action.type) {
-            case "one": 
-                return {years: state.years + 1};
-            case "two": 
-                return {years: state.years + action.play} 
-            case "three": 
-                return {years: state.years + 3} 
-            
+// Функция, которая:
+// 1. Имеет два аргумента (компонент, и доп. фунции)
+// 2. Содержит два возврата. Первый - логику компонента.
+// Второй - аргумент, который содержит компонент. Обязательно в этот
+// аргумент помещаем proops
+// Т.о. эта функция становится универсальной оберткой,
+// принимающей логику компонента.
+const withAllSlider = (Component, pullFunction) => {
+    return(props) => {
+        const [slide, setSlide] = useState(0);
+        const [autoplay, setAutoplay] = useState(false)
+    
+        useEffect(() => {
+            setSlide(pullFunction);
+        }, [])
+    
+        function changeSlide(i) {
+            setSlide(slide => slide + i);
         }
+        return(
+            <Component
+            {...props}  
+            slide= {slide} 
+            setSlide={setSlide}
+            autoplay={autoplay}
+            setAutoplay = {setAutoplay}
+            changeSlide = {changeSlide}
+            />
+        )
     }
-    return (
-        <> 
-            <button onClick={()=> dispatch({type: "one"})}>one</button>
-            <button onClick={
-                (e)=> dispatch(                    
-                    {type: "two", play: +e.target.textContent},                                       
-                )
-            }>100</button>
-            <button onClick={()=> dispatch({type: "three"})}>three</button>
-            <div>{years.years}</div>
-        </>
-    )            
 }
 
-export default App
+// Логику компонента закидываем через props
+const SliderFirst = (props) => {
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {props.slide}</div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(1)}>+1</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+const SliderSecond = (props) => {
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {props.slide} <br/>{props.autoplay ? 'auto' : null} </div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.setAutoplay(autoplay => !props.autoplay)}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+        //    Компоненты кладем в синтаксисе не как компоненты, а как фукнцию, вторую фукнцию не раскрываем
+const WithSliderFirst = withAllSlider(SliderFirst, getDataFromFirstFetch);
+const WithSliderSecond = withAllSlider(SliderSecond, getDataFromSecondFetch);
+
+function App() {
+    return (
+        <>
+        {/* Закидываем переменные, который будут содержать измененный компонент */}
+            <WithSliderFirst/>
+            <WithSliderSecond/>
+        </>
+    );
+}
+
+export default App;

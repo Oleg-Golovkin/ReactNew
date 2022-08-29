e4321ac5e81fb464e4214ffeea78f22c71b3fa7d
Oleg <olegjan9@rambler.ru>
2022-08-29 08:35:56 +0300

    26.08.2022

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,11 @@
 import {useState, useEffect} from 'react';
-import {Container} from 'react-bootstrap';
 import './App.css';
 
 
 const getDataFromFirstFetch = () => {return 10};
 const getDataFromSecondFetch = () => {return 20};
 
-// Функция, которая:
-// 1. Имеет два аргумента (компонент, и доп. фунции)
-// 2. Содержит два возврата. Первый - логику компонента.
-// Второй - аргумент, который содержит компонент. Обязательно в этот
-// аргумент помещаем proops
-// Т.о. эта функция становится универсальной оберткой,
-// принимающей логику компонента.
+
 const withAllSlider = (Component, pullFunction) => {
     return(props) => {
         const [slide, setSlide] = useState(0);
@@ -41,54 +34,59 @@ const withAllSlider = (Component, pullFunction) => {
 // Логику компонента закидываем через props
 const SliderFirst = (props) => {
     return (
-        <Container>
-            <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {props.slide}</div>
-                <div className="buttons mt-3">
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => props.changeSlide(-1)}>-1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => props.changeSlide(1)}>+1</button>
-                </div>
+        <div>
+            <div>
+                Active slide {props.slide}
             </div>
-        </Container>
+            <div>
+                <button                        
+                    onClick={() => props.changeSlide(-1)}>
+                        -1
+                </button>
+                <button 
+                    onClick={() => props.changeSlide(1)}>
+                        +1
+                </button>
+            </div>
+        </div>
     )
 }
 
 const SliderSecond = (props) => {
     return (
-        <Container>
-            <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {props.slide} <br/>{props.autoplay ? 'auto' : null} </div>
-                <div className="buttons mt-3">
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => props.changeSlide(-1)}>-1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => props.changeSlide(1)}>+1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => props.setAutoplay(autoplay => !props.autoplay)}>toggle autoplay</button>
-                </div>
+        <div>
+            <div>
+                Active slide {props.slide} 
+                <br/>{props.autoplay ? 'auto' : null
+            }</div>
+            <div>
+                <button 
+                    onClick={
+                        () => props.changeSlide(-1)}>-1
+                </button>
+                <button 
+                onClick={
+                    () => props.changeSlide(1)}>+1
+                </button>
+                <button 
+                    onClick={
+                        () => props.setAutoplay(autoplay => !props.autoplay)}>
+                    toggle autoplay
+                </button>
             </div>
-        </Container>
+        </div>
     )
 }
-        //    Компоненты кладем в синтаксисе не как компоненты, а как фукнцию, вторую фукнцию не раскрываем
-const WithSliderFirst = withAllSlider(SliderFirst, getDataFromFirstFetch);
-const WithSliderSecond = withAllSlider(SliderSecond, getDataFromSecondFetch);
+//Компоненты кладем в синтаксисе не как компоненты, а как фукнцию, вторую фукнцию не раскрываем
+const NewSliderFirst = withAllSlider(SliderFirst, getDataFromFirstFetch);
+const NewSliderSecond = withAllSlider(SliderSecond, getDataFromSecondFetch);
 
 function App() {
     return (
         <>
         {/* Закидываем переменные, который будут содержать измененный компонент */}
-            <WithSliderFirst/>
-            <WithSliderSecond/>
+            <NewSliderFirst/>
+            <NewSliderSecond/>
         </>
     );
 }

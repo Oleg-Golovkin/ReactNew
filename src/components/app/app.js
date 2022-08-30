85c5aed3190bf983d8a1219567ac6c9a406b52dd
Oleg <olegjan9@rambler.ru>
2022-08-30 22:29:58 +0300

    30.08.2022

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,96 +1,49 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
+import {Container} from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 
-
-const getDataFromFirstFetch = () => {return 10};
-const getDataFromSecondFetch = () => {return 20};
-
-
-const withAllSlider = (Component, pullFunction) => {
-    return(props) => {
-        const [slide, setSlide] = useState(0);
-        const [autoplay, setAutoplay] = useState(false)
-    
-        useEffect(() => {
-            setSlide(pullFunction);
-        }, [])
-    
-        function changeSlide(i) {
-            setSlide(slide => slide + i);
-        }
-        return(
-            <Component
-            {...props}  
-            slide= {slide} 
-            setSlide={setSlide}
-            autoplay={autoplay}
-            setAutoplay = {setAutoplay}
-            changeSlide = {changeSlide}
-            />
-        )
-    }
-}
-
-// Логику компонента закидываем через props
-const SliderFirst = (props) => {
+const Modal = (props) => {
+    console.log(props.showModal);
     return (
-        <div>
+        <CSSTransition 
+        in={props.showModal} 
+        timeout={300} 
+        
+        classNames="my-node">
             <div>
-                Active slide {props.slide}
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Typical modal window</h5>
+                        <button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <p>Modal body content</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
+                        <button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <button                        
-                    onClick={() => props.changeSlide(-1)}>
-                        -1
-                </button>
-                <button 
-                    onClick={() => props.changeSlide(1)}>
-                        +1
-                </button>
-            </div>
-        </div>
+        </CSSTransition>
     )
 }
-
-const SliderSecond = (props) => {
-    return (
-        <div>
-            <div>
-                Active slide {props.slide} 
-                <br/>{props.autoplay ? 'auto' : null
-            }</div>
-            <div>
-                <button 
-                    onClick={
-                        () => props.changeSlide(-1)}>-1
-                </button>
-                <button 
-                onClick={
-                    () => props.changeSlide(1)}>+1
-                </button>
-                <button 
-                    onClick={
-                        () => props.setAutoplay(autoplay => !props.autoplay)}>
-                    toggle autoplay
-                </button>
-            </div>
-        </div>
-    )
-}
-
-
-//Компоненты кладем в синтаксисе не как компоненты, а как фукнцию, вторую фукнцию не раскрываем
-const NewSliderFirst = withAllSlider(SliderFirst, getDataFromFirstFetch);
-const NewSliderSecond = withAllSlider(SliderSecond, getDataFromSecondFetch);
-
 
 function App() {
+    const [showModal, setShowModal] = useState(false);
+    console.log(showModal);
+    
     return (
-        <>
-        {/* Закидываем переменные, который будут содержать измененный компонент */}
-            <NewSliderFirst/>
-            <NewSliderSecond/>
-        </>
+        <Container>
+            <Modal onClose={setShowModal} showModal = {showModal}/>
+            <button 
+                type="button" 
+                className="btn btn-warning mt-5"
+                onClick={() => setShowModal(true)}>Open Modal</button>
+        </Container>
     );
 }
 
